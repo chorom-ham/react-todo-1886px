b9e2755cff5a10c2ce3c26fa1d57082851a69480
함초롬 <chorom.m.ham@gmail.com>
2021-09-11 18:44:46 +0900

    🎉 Initial settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from "styled-components";
+
+import Header from "./components/Header.js";
+import Todo from "./components/todo/Todo";
+
+const GlobalStyle = createGlobalStyle`
+  *{  
+    box-sizing: border-box;
+  }
+  html {
+    font-size : 10px;
+  }
+  body {
+    width: 100%;
+    background-color : #F6FFDB;
+    margin: 0;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <GlobalStyle />
+      <Header />
+      <Todo />
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
 
 export default App;
---
+++ src/components/todo/Form.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+// Hint: Form, Input, Button
+// 반드시 투두를 등록하는 버튼을 구현합니다.
+// 엔터를 눌렀을 때도 투두가 등록되도록 합니다.
+export default function Form() {
+  return <div>벗들 파이팅 :)</div>;
+}
