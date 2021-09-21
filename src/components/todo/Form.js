f03a8e9418cc9be0f292fc725ab6ea772cd44946
Yeonhee_Jung <spig0126@gmail.com>
2021-09-21 17:12:03 +0900

    feat: create ItemInput and AddItemButton in Form

--- src/components/todo/Form.js
+++ src/components/todo/Form.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {FaPlus} from "react-icons/fa";
 
 // Hint: Form, Input, Button
 // 반드시 투두를 등록하는 버튼을 구현합니다.
@@ -6,7 +7,10 @@ import styled from "styled-components";
 export default function Form() {
   return (
       <StyledForm>
-
+          <ItemInput type={'text'} placeholder={'할 일을 입력하세요'}/>
+          <AddItemButton>
+              <FaPlus />
+          </AddItemButton>
       </StyledForm>
   );
 }
@@ -17,4 +21,32 @@ const StyledForm = styled.div`
   border-radius: 1.6rem;
   height: 4rem;
   padding: 1rem;
+  align-items: center;
+`;
+
+const ItemInput = styled.input`
+  ::placeholder {
+    color: #bfcfff;
+  }
+
+  flex-grow: 1;
+  margin-right: 1rem;
+  border: none;
+  outline: none;
+  color: #bfcfff;
+  padding-left: 1.5rem;
+  font-style: italic;
+  background-color: transparent;
+`;
+
+const AddItemButton = styled.button`
+  background: transparent;
+  border: none;
+  transition-duration: 0.2s;
+  color: white;
+  padding-top: 0.2rem;
+  
+  &:hover {
+    color: rgb(215, 58, 246);
+  }
 `;
