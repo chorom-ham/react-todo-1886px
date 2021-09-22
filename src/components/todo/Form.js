27f2b2b1b48dac1decda6636a3e967e83e3a6e97
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-22 16:52:18 +0900

    Rename toDo to todo

--- src/components/todo/Form.js
+++ src/components/todo/Form.js
@@ -3,23 +3,22 @@ import styled from "styled-components";
 // Hint: Form, Input, Button
 // 반드시 투두를 등록하는 버튼을 구현합니다.
 // 엔터를 눌렀을 때도 투두가 등록되도록 합니다.
-export default function Form() {
+export default function Form(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
-    const toDoInput = event.target.firstChild;
-    console.log(toDoInput.value);
-    toDoInput.value = "";
+    const todoInput = event.target.firstChild;
+    todoInput.value = "";
   };
   return (
-    <ToDoForm onSubmit={handleSubmit}>
-      <ToDoInput type="text" placeholder="할 일을 입력하세요" />
+    <TodoForm onSubmit={handleSubmit}>
+      <TodoInput type="text" placeholder="할 일을 입력하세요" />
       <AddButton>추가</AddButton>
-    </ToDoForm>
+    </TodoForm>
   );
 }
 
-const ToDoForm = styled.form``;
+const TodoForm = styled.form``;
 
 const AddButton = styled.button``;
 
-const ToDoInput = styled.input``;
+const TodoInput = styled.input``;
