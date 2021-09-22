1baa85cd4c8e78d6c13b7b71e5326b19c0084818
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-22 18:20:40 +0900

    Add todo lists
    할 일 입력하면 목록에 추가

--- src/components/todo/Item.js
+++ src/components/todo/Item.js
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 
 // Hint: props로 id, text, onClick 등 (자유)
-export default function Item() {
-  return <div>벗들 파이팅 :)</div>;
+export default function Item({ todoList }) {
+  return (
+    <TodoLists>
+      {todoList.map((todo, index) => (
+        <TodoList key={todo.id}>
+          <Checkbox type="checkbox" id={index} />
+          <StyledLable htmlFor={index}>{todo.value}</StyledLable>
+        </TodoList>
+      ))}
+    </TodoLists>
+  );
 }
+
+const TodoLists = styled.div``;
+const TodoList = styled.div``;
+const Checkbox = styled.input``;
+const StyledLable = styled.label``;
