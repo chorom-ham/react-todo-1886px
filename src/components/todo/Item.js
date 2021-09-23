7ffd0e817904053ece8cfde35aafd5facc61b654
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-23 23:18:17 +0900

    Add click handler for toggle
    todo 클릭 시 스타일 변경

--- src/components/todo/Item.js
+++ src/components/todo/Item.js
@@ -3,19 +3,32 @@ import { IoCloseOutline } from "react-icons/io5";
 
 import styled from "styled-components";
 
-export default function Item({ todoList, deleteTodo }) {
+export default function Item({ todoList, deleteTodo, checkStyle }) {
   const todoLabel = useRef([]);
+  const checkbox = useRef([]);
 
   const handleDeleteClick = (index) => {
     const value = todoLabel.current[index].innerText;
     deleteTodo(value);
   };
 
+  const handleBoxClick = (index) => {
+    const { style } = todoLabel.current[index];
+    const { checked } = checkbox.current[index];
+    style.textDecoration = checked ? "line-through" : null;
+    style.color = checked ? "gray" : null;
+  };
+
   return (
     <TodoLists>
       {todoList.map((todo, index) => (
         <TodoList key={todo.id}>
-          <Checkbox type="checkbox" id={index} />
+          <Checkbox
+            ref={(box) => (checkbox.current[index] = box)}
+            type="checkbox"
+            id={index}
+            onClick={() => handleBoxClick(index)}
+          />
           <StyledLabel
             ref={(label) => (todoLabel.current[index] = label)}
             htmlFor={index}
