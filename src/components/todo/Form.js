be8a4e0c41c8ac2d65e64ee2e8466d4ce883871f
Yeonhee_Jung <spig0126@gmail.com>
2021-09-24 19:34:18 +0900

    feat: create 'changeInputText', 'AddItemToList', and 'onEnter' function

--- src/components/todo/Form.js
+++ src/components/todo/Form.js
@@ -1,19 +1,41 @@
 import styled from "styled-components";
 import {FaPlus} from "react-icons/fa";
+import Todo from "./Todo.js";
+import {useState} from "react";
 
 // Hint: Form, Input, Button
 // 반드시 투두를 등록하는 버튼을 구현합니다.
 // 엔터를 눌렀을 때도 투두가 등록되도록 합니다.
-export default function Form() {
-  return (
-      <StyledForm>
-          <ItemInput type={'text'} placeholder={'할 일을 입력하세요'}/>
-          <AddItemButton>
-              <FaPlus />
-          </AddItemButton>
-      </StyledForm>
-  );
-}
+export default function Form({todoList, setTodoList}) {
+    const [text, setText] = useState('');
+    const changeInputText = (e) => {
+        setText(e.target.value);
+    };
+
+    const addItemToList = (e) => {
+        const updatedList = [
+            ...todoList,
+            text
+        ]
+        setTodoList(updatedList);
+        setText('');
+    };
+
+    const onEnter = (e) => {
+        if (e.key == 'Enter') {
+            addItemToList();
+        }
+    };
+
+    return (
+        <StyledForm>
+            <ItemInput type={'text'} placeholder={'할 일을 입력하세요'} onChange={changeInputText} onKeyDown={onEnter} value={text}/>
+            <AddItemButton onClick={addItemToList}>
+                <FaPlus/>
+            </AddItemButton>
+        </StyledForm>
+    );
+};
 
 const StyledForm = styled.div`
   display: inline-flex;
@@ -22,6 +44,7 @@ const StyledForm = styled.div`
   height: 4rem;
   padding: 1rem;
   align-items: center;
+  margin: 1rem 0;
 `;
 
 const ItemInput = styled.input`
