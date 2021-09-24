73fc3d7b2907df803f5eeae060a055be951e7753
Yeonhee_Jung <spig0126@gmail.com>
2021-09-24 21:52:54 +0900

    push for vercel

--- src/components/todo/Form.js
+++ src/components/todo/Form.js
@@ -2,9 +2,6 @@ import styled from "styled-components";
 import {FaPlus} from "react-icons/fa";
 import {useState} from "react";
 
-// Hint: Form, Input, Button
-// 반드시 투두를 등록하는 버튼을 구현합니다.
-// 엔터를 눌렀을 때도 투두가 등록되도록 합니다.
 export default function Form({todoList, setTodoList}) {
     const [text, setText] = useState('');
     const changeInputText = (e) => {
