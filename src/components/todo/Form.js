1ff7121a0aedbb05286bd4b34c72200d435efa1e
songing01 <79829662+songing01@users.noreply.github.com>
2021-09-25 03:05:28 +0900

    get input value from onclick

--- src/components/todo/Form.js
+++ src/components/todo/Form.js
@@ -1,8 +1,24 @@
+import { useRef, useState } from "react";
 import styled from "styled-components";
 
 // Hint: Form, Input, Button
 // 반드시 투두를 등록하는 버튼을 구현합니다.
 // 엔터를 눌렀을 때도 투두가 등록되도록 합니다.
 export default function Form() {
-  return <div>벗들 파이팅 :)</div>;
+  const [inputItem, setInputItem] = useState("");
+  const inputId = useRef();
+  const addItem = (e) => {
+    setInputItem(inputId.current.value);
+  };
+
+  const items = [];
+  const todoList = items.map((id, item) => <li key={id}>{item}</li>);
+
+  return (
+    <div>
+      <input placeholder="할 일을 입력하세요" ref={inputId} />
+      <button onClick={addItem}>등록</button>
+      {todoList}
+    </div>
+  );
 }
--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -7,5 +7,9 @@ import Item from "./Item";
 export default function Todo() {
   const [todoList, setTodoList] = useState([]);
 
-  return <div>벗들 파이팅 :)</div>;
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 }
