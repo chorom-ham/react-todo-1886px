0bb072f96b404c76ae80cd970dfe268d97e48466
함초롬 <chorom.m.ham@gmail.com>
2021-09-26 00:36:25 +0900

    ✨ feat: Add example code

--- src/components/todo/Item.js
+++ src/components/todo/Item.js
@@ -1,6 +1,42 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-// Hint: props로 id, text, onClick 등 (자유)
-export default function Item() {
-  return <div>벗들 파이팅 :)</div>;
+export default function Item({ title, index, deleteItem }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const toggleChecked = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <Wrapper onClick={toggleChecked}>
+      <Text isChecked={isChecked}>
+        {index + 1}. {title}
+      </Text>
+      <DeleteButton type="button" onClick={() => deleteItem(index)}>
+        X
+      </DeleteButton>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Text = styled.p`
+  font-size: 2rem;
+  ${(props) =>
+    props.isChecked &&
+    `
+    text-decoration: line-through;
+    color: #ccc;
+  `}
+`;
+
+const DeleteButton = styled.button`
+  width: fit-content;
+  height: fit-content;
+  margin-left: 1rem;
+`;
