a75b4ddf3a797ab0891790cf28b3c4ca34bef352
march0320 <march18529@gmail.com>
2021-09-25 20:15:48 +0900

    Item

--- src/components/todo/Item.js
+++ src/components/todo/Item.js
@@ -1,6 +1,50 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 // Hint: props로 id, text, onClick 등 (자유)
-export default function Item() {
-  return <div>벗들 파이팅 :)</div>;
+function User({ todo, onRemove }) {
+  const [toggle, setToggle] = useState(false);
+
+  const onToggle = () => {
+    setToggle(toggle === false ? true : false);
+  };
+
+  return (
+    <Wrapper1>
+      <Text value={toggle} onClick={onToggle}>
+        {todo.text}
+      </Text>
+      <DeleteButton onClick={() => onRemove(todo.id)}>x</DeleteButton>
+    </Wrapper1>
+  );
 }
+
+export default function Item({ todoList, onRemove }) {
+  return (
+    <div>
+      {todoList.map((todo) => (
+        <User todo={todo} key={todo.id} onRemove={onRemove} />
+      ))}
+    </div>
+  );
+}
+const Wrapper1 = styled.div`
+  text-align: center;
+  margin-left: 7rem;
+  margin-top: 1rem;
+`;
+
+const Text = styled.text`
+  font-size: 1.5rem;
+  ${(todo) =>
+    todo.value === true
+      ? `text-decoration: line-through;`
+      : `text-decoration: none;`}
+`;
+
+const DeleteButton = styled.button`
+  margin-left: 0.5rem;
+  background-color: #ffebcd;
+  color: red;
+  border: 0.2rem;
+`;
