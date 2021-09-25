bcf49139e4d7301af387cfa17124f944bcfa6b1b
usernamebuzz <ohminji@ewhain.net>
2021-09-25 15:31:34 +0900

    line-through&delete

--- src/components/todo/Item.js
+++ src/components/todo/Item.js
@@ -1,6 +1,28 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 // Hint: props로 id, text, onClick 등 (자유)
-export default function Item() {
-  return <div>벗들 파이팅 :)</div>;
+export default function Item({ task, todoList, setTodoList }) {
+  const [state, setState] = useState("undone");
+  const changeState = () => {
+    setState(state === "undone" ? "done" : "undone");
+  };
+
+  const removeItem = (e) => {
+    setTodoList(todoList.filter((item) => item !== task));
+  };
+
+  return (
+    <section>
+      <ul>
+        <li
+          style={{ textDecoration: task === "none" ? "line-through" : "none" }}
+          onClick={changeState}
+        >
+          {task}
+        </li>
+        <button value={state} onClick={removeItem}></button>
+      </ul>
+    </section>
+  );
 }
