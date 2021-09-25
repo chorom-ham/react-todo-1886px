ea9f16dea02c474a7c9e6c533d8d9bfc2d9bb7fe
spiritstone <mjk4534@gmail.com>
2021-09-25 20:54:23 +0900

    Todo 단위 변경 및 코드 작성 완료

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,11 +1,32 @@
+import React from "react";
 import { useState } from "react";
 import styled from "styled-components";
 
 import Form from "./Form";
 import Item from "./Item";
 
-export default function Todo() {
-  const [todoList, setTodoList] = useState([]);
 
-  return <div>벗들 파이팅 :)</div>;
+export default function Todo() {
+  const todos = useTodoState();
+  const undoneTasks = todos.filter((todo) => !todo.done);
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
+
+  return (
+    <>
+      <Item />
+      <Form />
+      <h1>{dateString}</h1>
+      <div className="day">{dayName}</div>
+      <div className="tasks-left"> {undoneTasks.length} Todos Left</div>
+    </>
+  );
 }
+
+export default React.memo(Todo);
