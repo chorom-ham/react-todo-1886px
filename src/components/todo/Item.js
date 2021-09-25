5c59b3fc167db7ec8456aa1c22f3780478540d17
spiritstone <mjk4534@gmail.com>
2021-09-25 20:53:47 +0900

    css 단위 rem으로 변경

--- src/components/todo/Item.js
+++ src/components/todo/Item.js
@@ -2,14 +2,13 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { MdDone, MdDelete } from "react-icons/md";
-import { useTodoDispatch } from "../Todo";
 
 const Remove = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   color: #dee2e6;
-  font-size: 24px;
+  font-size: 2rem;
   cursor: pointer;
   &:hover {
     color: #ff6b6b;
@@ -20,8 +19,8 @@ const Remove = styled.div`
 const TodoItemBlock = styled.div`
   display: flex;
   align-items: center;
-  padding-top: 12px;
-  padding-bottom: 12px;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
   &:hover {
     ${Remove} {
       display: initial;
@@ -30,27 +29,27 @@ const TodoItemBlock = styled.div`
 `;
 
 const CheckCircle = styled.div`
-  width: 32px;
-  height: 32px;
-  border-radius: 16px;
-  border: 1px solid #ced4da;
-  font-size: 24px;
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 1.3rem;
+  border: 0.01rem solid #ced4da;
+  font-size: 2rem;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-right: 20px;
+  margin-right: 1.8rem;
   cursor: pointer;
   ${(props) =>
     props.done &&
     css`
-      border: 1px solid #38d9a9;
+      border: 0.01rem solid #38d9a9;
       color: #38d9a9;
     `}
 `;
 
 const Text = styled.div`
   flex: 1;
-  font-size: 21px;
+  font-size: 2rem;
   color: #495057;
   ${(props) =>
     props.done &&
@@ -76,4 +75,4 @@ function Item({ id, done, text }) {
   );
 }
 
-export default React.memo(TodoItem);
+export default Item;
