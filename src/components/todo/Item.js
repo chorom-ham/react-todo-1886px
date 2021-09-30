08f230e6393dbf00a78b0c056e4ffaf88094a5b3
songing01 <79829662+songing01@users.noreply.github.com>
2021-09-30 16:15:57 +0900

    create toggle function

--- src/components/todo/Item.js
+++ src/components/todo/Item.js
@@ -1,15 +1,27 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import styled from "styled-components";
 import Todo from "./Todo";
 // Hint: props로 id, text, onClick 등 (자유)
 export default function Item({ title, index, deleteItem }) {
+  const [isChecked, setIsChecked] = useState(false);
+  const toggleisChecked = () => {
+    setIsChecked(!isChecked);
+  };
   return (
-    <Wrapper>
-      {index + 1}.{title}
+    <Wrapper onClick={toggleisChecked}>
+      <Text isChecked={isChecked}>
+        {index + 1}.{title}
+      </Text>
       <DeleteButton onClick={() => deleteItem(index)}>X</DeleteButton>
     </Wrapper>
   );
 }
 const Wrapper = styled.div``;
-const Text = styled.div``;
+const Text = styled.div`
+  ${(props) =>
+    props.isChecked &&
+    `
+  text-decoration: line-through;
+  `}
+`;
 const DeleteButton = styled.button``;
