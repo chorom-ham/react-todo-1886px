f38f970cc4aba8292ea68765fe4446fe9828deb5
wishJinit <wishjinit@gmail.com>
2022-04-11 16:05:07 +0900

    Add Features

--- src/components/todo/Form.js
+++ src/components/todo/Form.js
@@ -1,22 +1,30 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 // Hint: Form, Input, Button
 // 반드시 투두를 등록하는 버튼을 구현합니다.
 // 엔터를 눌렀을 때도 투두가 등록되도록 합니다.
 
-export default function Form() {
+export default function Form({ addTodoList }) {
+  const [message, setMessage] = useState("");
+
   return (
-    <TodoForm>
-      <TextInput placeholder="할 일을 입력하세요" />
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        addTodoList(message, false);
+      }}
+    >
+      <TextInput
+        placeholder="할 일을 입력하세요"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
       <SubmitButton>추가</SubmitButton>
-    </TodoForm>
+    </form>
   );
 }
 
-const TodoForm = styled.form`
-  width: 95%;
-  margin: 3rem auto;
-`;
 const TextInput = styled.input.attrs((props) => ({
   type: "text",
 }))`
@@ -25,6 +33,7 @@ const TextInput = styled.input.attrs((props) => ({
   padding: 1rem;
   font-size: 2rem;
 `;
+
 const SubmitButton = styled.button.attrs((props) => ({
   type: "submit",
 }))`
